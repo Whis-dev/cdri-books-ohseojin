2f077d3fbb17a79683b71c8dbc54b09acdf701a5
whis <whisdevj@gmail.com>
2025-06-16 05:44:47 +0900

    feat(App): add Browser Router (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { ThemeProvider } from "@emotion/react"
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router';
 
 import DEFAULT_THEME from './constants/theme';
+import GlobalStyle from './components/common/GlobalStyle';
 
 function App() {
   return (
-   <ThemeProvider theme={DEFAULT_THEME}>
-    <></>
-   </ThemeProvider>
-  )
+    <BrowserRouter>
+      <GlobalStyle />
+      <ThemeProvider theme={DEFAULT_THEME}>
+        <></>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
