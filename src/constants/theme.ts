e2e2a4b29226ca8a5219be07af43a81dec014d27
whis <whisdevj@gmail.com>
2025-06-16 19:01:25 +0900

    feat(constants > theme): add typography variant style for typography component

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,12 +1,12 @@
 const TYPOGRAPHY = Object.freeze({
   fontSize: {
-    xxl: "24px",
-    xl: "22px",
-    lg: "20px",
-    md: "18px",
-    sm: "16px",
-    xs: "14px",
-    xxs: "10px"
+    xxl: '24px',
+    xl: '22px',
+    lg: '20px',
+    md: '18px',
+    sm: '16px',
+    xs: '14px',
+    xxs: '10px',
   },
 
   fontWeight: {
@@ -15,35 +15,84 @@ const TYPOGRAPHY = Object.freeze({
   },
 
   lineHeight: {
-    xxl: "24px",
-    lg: "20px",
-    md: "18px",
-    sm: "16px",
-    xs: "14px",
-    xxs: "10px"
+    xxl: '24px',
+    lg: '20px',
+    md: '18px',
+    sm: '16px',
+    xs: '14px',
+    xxs: '10px',
   },
+});
 
-}) 
+const TYPOGRAPHY_VARIANT_STYLE: Record<
+  string,
+  {
+    fontSize: keyof typeof TYPOGRAPHY.fontSize;
+    fontWeight: keyof typeof TYPOGRAPHY.fontWeight;
+    lineHeight: keyof typeof TYPOGRAPHY.lineHeight;
+  }
+> = Object.freeze({
+  title1: {
+    fontSize: 'xxl',
+    fontWeight: 'bold',
+    lineHeight: 'xxl',
+  },
+  title2: {
+    fontSize: 'xl',
+    fontWeight: 'bold',
+    lineHeight: 'xxl',
+  },
+  title3: {
+    fontSize: 'md',
+    fontWeight: 'bold',
+    lineHeight: 'md',
+  },
+  body1: {
+    fontSize: 'lg',
+    fontWeight: 'regular',
+    lineHeight: 'lg',
+  },
+  body2: {
+    fontSize: 'xs',
+    fontWeight: 'regular',
+    lineHeight: 'xs',
+  },
+  body2bold: {
+    fontSize: 'xs',
+    fontWeight: 'bold',
+    lineHeight: 'xs',
+  },
+  caption: {
+    fontSize: 'sm',
+    fontWeight: 'regular',
+    lineHeight: 'sm',
+  },
+  small: {
+    fontSize: 'xxs',
+    fontWeight: 'regular',
+    lineHeight: 'xxs',
+  },
+});
 
 const COLOR = Object.freeze({
   palette: {
-    primary: "#4880EE",
-    red: "#E84118",
-    gray: "#DADADA",
-    lightGray: "#F2F4F6",
-    white: "#FFFFFF",
-    black: "#222222",},
+    primary: '#4880EE',
+    red: '#E84118',
+    gray: '#DADADA',
+    lightGray: '#F2F4F6',
+    white: '#FFFFFF',
+    black: '#222222',
+  },
   text: {
-    primary: "#353C49",
-    secondary: "#6D7582",
-    subtitle: "#8D94A0"
-  }  
-})
+    primary: '#353C49',
+    secondary: '#6D7582',
+    subtitle: '#8D94A0',
+  },
+});
 
 const DEFAULT_THEME = Object.freeze({
   typography: TYPOGRAPHY,
   color: COLOR,
-})
+});
 
-
-export default DEFAULT_THEME
+export { DEFAULT_THEME as default, TYPOGRAPHY_VARIANT_STYLE };
