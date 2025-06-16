c14f752fe61c9e748814c4afd5cac06ddec15354
whis <whisdevj@gmail.com>
2025-06-16 19:03:07 +0900

    feat(components > ... > Typography): add Typography component

--- src/components/common/data-display/Typography.tsx
+++ src/components/common/data-display/Typography.tsx
@@ -1,3 +1,40 @@
-export default function Typography() {
-  return <span></span>;
+import type { PropsWithChildren } from 'react';
+import styled from '@emotion/styled';
+
+import { TYPOGRAPHY_VARIANT_STYLE } from '@/constants/theme';
+
+interface ITypographyProps extends PropsWithChildren {
+  variant?: keyof typeof TYPOGRAPHY_VARIANT_STYLE;
+}
+
+const StyledTypography = styled.span<{
+  variant: keyof typeof TYPOGRAPHY_VARIANT_STYLE;
+}>`
+  font-size: ${props =>
+    props.theme.typography.fontSize[
+      TYPOGRAPHY_VARIANT_STYLE[props.variant].fontSize
+    ]};
+  font-weight: ${props =>
+    props.theme.typography.fontWeight[
+      TYPOGRAPHY_VARIANT_STYLE[props.variant].fontWeight
+    ]};
+  line-height: ${props =>
+    props.theme.typography.lineHeight[
+      TYPOGRAPHY_VARIANT_STYLE[props.variant].lineHeight
+    ]};
+  letter-spacing: 0;
+`;
+
+export default function Typography({
+  variant = 'body1',
+  children,
+}: ITypographyProps) {
+  return (
+    <StyledTypography
+      variant={variant}
+      as={/title/.test(variant) ? 'h2' : 'span'}
+    >
+      {children}
+    </StyledTypography>
+  );
 }
