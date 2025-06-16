b48de485988046faddec9904945e7f614b55176d
whis <whisdevj@gmail.com>
2025-06-16 19:03:52 +0900

    feat(*): change Main component to Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router';
 
-import DEFAULT_THEME from './constants/theme';
-import GlobalStyle from './components/common/GlobalStyle';
+import DEFAULT_THEME from '@/constants/theme';
+import GlobalStyle from '@/components/common/GlobalStyle';
+import Wrapper from '@/components/common/layout/Wrapper';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <ThemeProvider theme={DEFAULT_THEME}>
-        <></>
+        <Wrapper />
       </ThemeProvider>
     </BrowserRouter>
   );
---
+++ src/components/common/layout/Wrapper.tsx
@@ -0,0 +1,13 @@
+import Typography from '../data-display/Typography';
+
+export default function Main() {
+  return (
+    <>
+      <header>
+        <Typography variant="title1">book</Typography>
+      </header>
+
+      <main></main>
+    </>
+  );
+}
