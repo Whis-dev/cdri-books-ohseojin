e1517c90072712b50ee98b637891017b95f9c04b
whis <whisdevj@gmail.com>
2025-06-22 20:31:25 +0900

    feat(components > ... > Wrapper): set main layout
    - add tab navigation for moving page

--- src/components/common/layout/Wrapper.tsx
+++ src/components/common/layout/Wrapper.tsx
@@ -1,13 +1,49 @@
-import Typography from '../data-display/Typography';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router';
+
+import Typography from '@/components/common/data-display/Typography';
+import Tab from '@/components/common/navigator/Tab';
+
+const TAB_ITEMS = [
+  {
+    key: 'search-book',
+    label: '도서 검색',
+    to: '/',
+  },
+  {
+    key: 'favorite-book',
+    label: '내가 찜한 책',
+    to: '/favorite-book',
+  },
+];
 
 export default function Main() {
+  const [selectedTab, setSelectedTab] = useState<
+    'search-book' | 'favorite-book'
+  >('search-book');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setSelectedTab(location.pathname === '/' ? 'search-book' : 'favorite-book');
+  }, [location.pathname]);
+
   return (
     <>
       <header>
-        <Typography variant="title1">book</Typography>
+        <Typography variant="title1">CERTICOS BOOKS</Typography>
+
+        <Tab
+          isLinkTab
+          fullWidth
+          selectedTab={selectedTab}
+          tabItems={TAB_ITEMS}
+        />
       </header>
 
-      <main></main>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
