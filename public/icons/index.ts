76675a32a7f9180162eef2ebf930e587b85a12bc
whis <whisdevj@gmail.com>
2025-06-22 20:21:11 +0900

    assets(icons): add icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,4 +1,18 @@
-import LikeLineIcon from './like-line.svg';
-import LikeFillIcon from './like-fill.svg';
+import BookIcon from './book.svg?react';
 
-export { LikeLineIcon, LikeFillIcon };
+import ChevronDownIcon from './chevron-down.svg?react';
+import ChevronUpIcon from './chevron-up.svg?react';
+import CloseIcon from './close.svg?react';
+import LikeLineIcon from './like-line.svg?react';
+import LikeFillIcon from './like-fill.svg?react';
+import SearchIcon from './search.svg?react';
+
+export {
+  BookIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CloseIcon,
+  LikeLineIcon,
+  LikeFillIcon,
+  SearchIcon,
+};
