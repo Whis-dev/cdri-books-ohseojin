f9c6c96c9c3256f9a9a6fa1c651e06d9f2a6221d
whis <whisdevj@gmail.com>
2025-06-22 20:25:07 +0900

    feat(components > ... > GlobalStyle): add some style to GlobalStyle

--- src/components/common/GlobalStyle.tsx
+++ src/components/common/GlobalStyle.tsx
@@ -7,7 +7,7 @@ export default function GlobalStyle() {
     <Global
       styles={css`
         @font-face {
-          font-family: 'Noto Sans KR";
+          font-family: 'Noto Sans KR';
           src: url(${NotoSansKRMedium}) format('ttf');
           font-display: swap;
         }
@@ -16,15 +16,50 @@ export default function GlobalStyle() {
           margin: 0;
           padding: 0;
           font-family: 'Noto Sans KR';
+
+          scrollbar-width: thin;
         }
 
-        html, body {
+        html,
+        body {
           height: 100%;
           width: 100%;
+          overflow: hidden;
         }
 
-        ol, ul {
+        header {
+          display: flex;
+          width: calc(100% - 320px);
+          height: 80px;
+          padding: 0px 160px;
+          align-items: center;
+        }
+
+        main {
+          width: 960px;
+          height: calc(100vh - 80px - 160px);
+          margin: 0 auto;
+          padding: 80px 0px;
+          overflow: hidden;
+        }
+
+        a {
+          text-decoration: none;
+        }
+
+        ol,
+        ul {
           list-style: none;
+        }
+
+        i {
+          font-style: normal;
+        }
+
+        button {
+          &:hover {
+            cursor: pointer;
+          }
         }
       `}
     />
